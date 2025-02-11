6b058187584e8c29db0fe358db7d4d6ace3a65ac
Vibhu Vanjari <vibhu.vanjari@kellogg.northwestern.edu>
2025-02-11 20:55:12 +0000

    made it dynamic

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,6 +1,7 @@
 class TacosController < ApplicationController
   def index
     #...
-    render :inline => "<h1>Hello World!</h1>"
+    #render :inline => "<h1>Hello World!</h1>"
+    render :template => "tacos/index"
   end
 end
